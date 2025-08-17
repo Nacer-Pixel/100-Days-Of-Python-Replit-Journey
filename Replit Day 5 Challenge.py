38a6870bdb63f6dc69d2a50496ad2770aa8dc4a8
Nacer_Pixel <nacereddine@hotmail.fr>
2025-08-17 03:43:55 +0100

    Add Day 5 project

---
+++ Replit Day 5 Challenge.py
@@ -0,0 +1,28 @@
+print("Which Marvel character are you!")
+print()
+Spiderman = input("Do you like to swing on buildings? Answer by y/n ")
+if Spiderman == "y":
+  print()
+  print("You are Spiderman!")
+else: 
+  print()
+  Hulk = input("Do you get angry easily? Answer by y/n ")
+  if Hulk == "y":
+    print()
+    print("You are Hulk!")
+  else:
+    print()
+    Captain_America = input("Do you like shields? Answer by y/n ")
+    if Captain_America == "y":
+      print()
+      print("You are Captain America!")
+    else:
+      print()
+      Antman = input("Do you like ants? Answer by y/n ")
+      if Antman == "y":
+        print()
+        print("You are Antman!")
+      else:
+        print()
+        print("You're just a regular citizen :/")
+      
---
+++ Replit Day 5 Log in.py
@@ -0,0 +1,11 @@
+Username = input("Username: ")
+#Use the username Nacer
+if Username == "Nacer":
+  Password = input ("Password: ")
+  if Password == "123":
+    #Use the password 123
+    print("Welcome back Nacer!")
+  else:
+    print("Wrong Password try again later. ")
+else: 
+  print("Wrong Username try again later.")
