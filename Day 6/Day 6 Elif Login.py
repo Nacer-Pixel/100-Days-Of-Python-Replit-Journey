d974e9c9986f4b4858f364751140433aeeb704ee
Nacer_Pixel <nacereddine@hotmail.fr>
2025-08-18 03:46:59 +0100

    Added Day 6

---
+++ Day 6/Day 6 Elif Login.py
@@ -0,0 +1,12 @@
+print("MY LOGIN SYSTEM")
+print()
+USER = input("Enter your username: ")
+PASS = input("Enter your password: ")
+if USER == "Nacer" and PASS == "123":
+  print("Welcome back Nacer Have a nice day!")
+elif USER == "Joseph" and PASS == "JOSE":
+  print("Good Morning Joseph!")
+elif USER == "Maria" and PASS == "MAR":
+  print("Hello Maria welcome back!")
+else:
+  print("Wrong credentials please try again.")
